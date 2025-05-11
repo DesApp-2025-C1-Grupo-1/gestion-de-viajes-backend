8423558a58c69d52573719d4df4b4db04547a26d
Ari <ariel.paz.dev@gmail.com>
2025-05-11 18:11:03 -0300

    rm unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TipoVehiculoModule } from './tipo_vehiculo/tipo_vehiculo.module';
@@ -9,12 +7,7 @@ import { EmpresaModule } from './empresa/empresa.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    TipoVehiculoModule,
-    DatabaseModule,
-    VehiculoModule,
-    EmpresaModule,
-  ],
+  imports: [TipoVehiculoModule, DatabaseModule, VehiculoModule, EmpresaModule],
   controllers: [AppController],
   providers: [AppService],
 })
